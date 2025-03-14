c3c868e309cb7090d8ecf95cf9fc5231f035e2c6
Askari <askarir64@gmail.com>
2025-03-15 02:08:12 +0530

    fixed data fetching bug

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,25 +1,27 @@
 import { createClient, groq } from "next-sanity";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export async function getBlogs() {
+  const client = createClient({
+    projectId,
+    dataset,
+    apiVersion: "2025-03-14",
+    useCdn: true,
+  });
 
-    const client = createClient({
-        projectId,
-        dataset,
-        apiVersion: '2025-03-14'
-    })
-
-    return client.fetch(
-        groq`*[_type=='Blogs']{
+  const blogs = await client.fetch(
+    groq`*[_type == 'blogs']{
         _id,
         _createdAt,
         title,
         author,
         "slug": slug.current,
-        "image": image.asset-> url,
+        "image": image.asset -> url,
         content
         }`
-    )
+  );
+
+  return blogs;
 }
