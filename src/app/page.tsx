487fda7e560bb0226c66d7af420cb699a4c156b7
Askari <askarir64@gmail.com>
2025-04-26 20:09:51 +0530

    Rendering more data WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,37 @@
+import Image from "next/image";
 import { getBlogs } from "../../sanity/sanity-utils";
 
 export default async function Home() {
-  const blogs = await getBlogs()
+  const blogs = await getBlogs();
+  console.log(blogs);
   return (
     <>
-    <h1>Blogs</h1>
-    <br/>
-    <div>
-      {blogs.map((blog)=>(
-        <h3 key={blog._id}>{blog.title}</h3>
-      ))}
-    </div>
+      <h1>Blogs</h1>
+      <br />
+      <div>
+        {blogs.map((blog) => (
+          <div key={blog._id}>
+            <h2>{blog.title}</h2>
+            <Image src={blog.image} height={300} width={400} alt={blog.slug} />
+            <p>
+              Published: <span>{blog._createdAt}</span>
+            </p>
+            <br></br>
+            {blog.author.image && (
+              <Image
+                src={blog.author.image}
+                height={60}
+                width={60}
+                alt={blog.author.name}
+              />
+            )}
+            <p>
+              Author: <span>{blog.author.name}</span>
+            </p>
+            <h3></h3>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
