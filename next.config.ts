e98a5b7c2e095b7d9b6d62c3697c9954d52703f7
Askari <askarir64@gmail.com>
2025-04-29 02:26:43 +0530

    Next Config Update
    Using images.remotePatterns instead of images.domains

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+      },
+    ],
   },
-
 };
 
 export default nextConfig;
