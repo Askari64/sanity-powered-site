b61afd78d2c1b8d251e11e2b587e5bf39c89f65b
Askari <askarir64@gmail.com>
2025-05-03 14:08:04 +0530

    WIP Blog Card integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import {PortableText} from '@portabletext/react'
 import { getBlogs } from "../../sanity/sanity-utils";
+import BlogCard from "@/components/BlogCard";
 
 export default async function Home() {
   const blogs = await getBlogs();
@@ -12,24 +11,7 @@ export default async function Home() {
       <div>
         {blogs.map((blog) => (
           <div key={blog._id}>
-            <h2>{blog.title}</h2>
-            <Image src={blog.image} height={300} width={400} alt={blog.slug} />
-            <p>
-              Published: <span>{blog._createdAt}</span>
-            </p>
-            <br></br>
-            {blog.author.image && (
-              <Image
-                src={blog.author.image}
-                height={60}
-                width={60}
-                alt={blog.author.name}
-              />
-            )}
-            <p>
-              Author: <span>{blog.author.name}</span>
-            </p>
-            <PortableText value={blog.content}/>
+           <BlogCard imgAlt={blog.slug} imgSrc={blog.image} blogTitle={blog.title} />
           </div>
         ))}
       </div>
